559cb85e4ed4410bb7131d51c06dfabe0fb31601
duonglele <leeminh03062005@gmail.com>
2025-12-16 13:50:44 +0700

    Add files via upload

---
+++ Week4/db_connect.php
@@ -0,0 +1,5 @@
+<?php
+    $link = mysqli_connect("localhost", "root", "", "login_demo", "3307") or die(mysqli_connect_error());
+
+    // mysqli_select_db($link, "login_demo") or die(mysqli_error($link));
+?>
